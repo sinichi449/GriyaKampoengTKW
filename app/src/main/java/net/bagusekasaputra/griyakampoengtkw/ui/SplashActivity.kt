a016e4a5f93c2f33f4e6b4aae996d32e87136dc9
sinichi449 <bagusekasaputra460@gmail.com>
2022-10-29 18:42:14 +0700

    fixed dark theme issue, blackening the data diri card text.

--- app/src/main/java/net/bagusekasaputra/griyakampoengtkw/ui/SplashActivity.kt
+++ app/src/main/java/net/bagusekasaputra/griyakampoengtkw/ui/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.AndroidEntryPoint
 import net.bagusekasaputra.griyakampoengtkw.databinding.ActivitySplashBinding
 import net.bagusekasaputra.griyakampoengtkw.ui.main.MainActivity
@@ -17,6 +18,9 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Disable Dark Theme
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         showSplashScreen(1.5f)
     }
