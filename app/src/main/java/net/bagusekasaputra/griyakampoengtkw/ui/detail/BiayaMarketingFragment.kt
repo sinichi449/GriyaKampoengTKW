019bafd28a11c5fcab673e5acca3041c134874d9
sinichi449 <bagusekasaputra460@gmail.com>
2022-11-15 08:18:57 +0700

    fix data diri no telp ketutupan maneh

--- app/src/main/java/net/bagusekasaputra/griyakampoengtkw/ui/detail/BiayaMarketingFragment.kt
+++ app/src/main/java/net/bagusekasaputra/griyakampoengtkw/ui/detail/BiayaMarketingFragment.kt
@@ -6,18 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
-import net.bagusekasaputra.griyakampoengtkw.databinding.FragmentFeeMarketingBinding
+import net.bagusekasaputra.griyakampoengtkw.databinding.FragmentBiayaMarketingBinding
 
 @AndroidEntryPoint
 class BiayaMarketingFragment : Fragment() {
 
-    private lateinit var binding: FragmentFeeMarketingBinding
+    private lateinit var binding: FragmentBiayaMarketingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = FragmentFeeMarketingBinding.inflate(inflater, container, false)
+        binding = FragmentBiayaMarketingBinding.inflate(inflater, container, false)
         return binding.root
     }
 
