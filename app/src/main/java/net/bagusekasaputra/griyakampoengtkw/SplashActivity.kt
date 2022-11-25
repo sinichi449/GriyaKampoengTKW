957e106d2c233b4417329658d1a97fe2e411eed7
sinichi449 <bagusekasaputra460@gmail.com>
2022-11-25 20:28:07 +0700

    added maintenance status on splash activity

--- app/src/main/java/net/bagusekasaputra/griyakampoengtkw/SplashActivity.kt
+++ app/src/main/java/net/bagusekasaputra/griyakampoengtkw/SplashActivity.kt
@@ -4,15 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.getValue
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import net.bagusekasaputra.griyakampoengtkw.databinding.ActivitySplashPureBinding
 import net.bagusekasaputra.griyakampoengtkw.databinding.ActivitySplashWithLoadingBinding
 import net.bagusekasaputra.griyakampoengtkw.presentation.R
@@ -54,9 +63,40 @@ class SplashActivity : AppCompatActivity() {
             setContentView(bindingLoading.root)
 
             CoroutineScope(Dispatchers.IO).launch {
-                deviceOnline().collect {
-                    goToMainActivity(it)
+                deviceOnline().collect { online ->
+                    if (online) {
+                        // Check Maintenance status
+                        val isMaintenance = checkMaintenance()
+
+                        isMaintenance.onSuccess { maintenance ->
+                            if (maintenance) {
+                                withContext(Dispatchers.Main) {
+                                    MaterialAlertDialogBuilder(this@SplashActivity)
+                                        .setTitle("Server Maintenance")
+                                        .setCancelable(false)
+                                        .setMessage("Mohon maaf, untuk saat ini server sedang menjalani proses pemeliharaan. Silakan coba lagi nanti.")
+                                        .setPositiveButton("Oke") { dialog, _ ->
+                                            dialog.dismiss()
+                                            this@SplashActivity.finish()
+                                        }
+                                        .create()
+                                        .show()
+                                }
+                            }
+                        }
+
+                        isMaintenance.onFailure {
+                            withContext(Dispatchers.Main) {
+                                Toast.makeText(this@SplashActivity, "Gagal mengecek status server: $it", Toast.LENGTH_LONG).show()
+                            }
+
+                            goToMainActivity(online)
+                        }
+                    } else {
+                        goToMainActivity(online)
+                    }
                 }
+
             }
         }
         // Convert integer to long milliseconds
@@ -91,5 +131,24 @@ class SplashActivity : AppCompatActivity() {
         intent.putExtra("versionCode", BuildConfig.VERSION_CODE)
         startActivity(intent)
         finish()
+    }
+
+    private suspend fun checkMaintenance(): Result<Boolean> {
+        return callbackFlow<Result<Boolean>> {
+            val database = FirebaseDatabase.getInstance(GriyaNodes.firebaseUrl)
+            val maintenanceRef = database.reference.child("maintenance")
+
+            maintenanceRef
+                .get()
+                .addOnSuccessListener { snapshot ->
+                    val statusServer = snapshot.getValue<Boolean>()
+                    trySendBlocking(Result.success(statusServer ?: false))
+                }
+                .addOnFailureListener {
+                    trySendBlocking(Result.failure(it))
+                }
+
+            awaitClose {  }
+        }.first()
     }
 }
