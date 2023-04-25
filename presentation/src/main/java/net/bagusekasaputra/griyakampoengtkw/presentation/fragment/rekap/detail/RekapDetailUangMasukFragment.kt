f86540ed704480062cf5835713aa5ffb9c6dcaee
sinichi449 <bagusekasaputra460@gmail.com>
2023-04-25 21:01:13 +0700

    view data lama oke

--- presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/fragment/rekap/detail/RekapDetailUangMasukFragment.kt
+++ presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/fragment/rekap/detail/RekapDetailUangMasukFragment.kt
@@ -42,23 +42,33 @@ class RekapDetailUangMasukFragment : Fragment() {
         viewModel.rekapBesarDetailLive.observe(requireActivity()) {
             it?.also { rekapBesarDetail ->
                 setupTableViewDataBaru(rekapBesarDetail.mapListPembayaranRekapBaru)
-
                 val rupiahTotalUangMasukRekapBaru = "Rp. ${NumberUtil.formatLongToString(rekapBesarDetail.getTotalUangMasukRekapBaru())}"
                 binding.tvTotalRekap.text = rupiahTotalUangMasukRekapBaru
+
+
+                val dataLamaIncluded = viewModel.rekapDetailTransportLive.value?.includeDataLama
+                if (dataLamaIncluded == true) {
+                    binding.layoutDataLama.visibility = View.VISIBLE
+                    binding.tvInfoDataBaru.visibility = View.VISIBLE
+                    setupTableViewDataLama(rekapBesarDetail.mapListPembayaranRekapLama)
+
+                    val rupiahTotalUangMasukRekapLama = "Rp. ${NumberUtil.formatLongToString(rekapBesarDetail.getTotalUangMasukRekapLama())}"
+                    binding.tvTotalRekapDataLama.text = rupiahTotalUangMasukRekapLama
+                } else {
+                    binding.layoutDataLama.visibility = View.GONE
+                    binding.tvInfoDataBaru.visibility = View.GONE
+                }
             }
         }
     }
 
     private fun setupTableViewDataBaru(mapListPembayaranBaru: Map<String, List<PembayaranWithNamaCostumer>?>) {
         val adapter = RekapUangMasukTableViewAdapter()
-        binding.tableviewRekapUangMasuk.apply {
-            setAdapter(adapter)
-        }
+        binding.tableviewRekapUangMasuk.setAdapter(adapter)
 
         val columnHeader = listOf(
             RumColumnHeader("Nama Costumer"),
             RumColumnHeader("Tanggal"),
-            // TODO: Nama Costumer
             RumColumnHeader("Jenis Pembayaran"),
             RumColumnHeader("Jumlah Pembayaran"),
         )
@@ -92,9 +102,59 @@ class RekapDetailUangMasukFragment : Fragment() {
 
         adapter.setAllItems(columnHeader, rowHeaders, cellLists)
         binding.tableviewRekapUangMasuk.apply {
-            setColumnWidth(0, 300) // Tanggal
-            setColumnWidth(1, 300) // Jenis Pembayaran
-            setColumnWidth(2, 350) // Jumlah Pembayaran
+            setColumnWidth(0, 350) // Nama Costumer
+            setColumnWidth(1, 300) // Tanggal
+            setColumnWidth(2, 300) // Jenis Pembayaran
+            setColumnWidth(3, 350) // Jumlah Pembayaran
+        }
+
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun setupTableViewDataLama(mapListPembayaranLama: Map<String, List<PembayaranWithNamaCostumer>?>) {
+        val adapter = RekapUangMasukTableViewAdapter()
+        binding.tableviewRekapUangMasukDataLama.setAdapter(adapter)
+
+        val columnHeader = listOf(
+            RumColumnHeader("Nama Costumer"),
+            RumColumnHeader("Tanggal"),
+            RumColumnHeader("Jenis Pembayaran"),
+            RumColumnHeader("Jumlah Pembayaran"),
+        )
+        val rowHeaders = mutableListOf<RumRowHeader>().apply {
+            var index = 1
+            mapListPembayaranLama.keys.forEach { kavling ->
+                mapListPembayaranLama[kavling]?.forEach {
+                    add(RumRowHeader(index.toString(), kavling))
+
+                    index++
+                }
+            }
+        }
+        val cellLists = mutableListOf<List<RumCell>>().apply {
+            mapListPembayaranLama.keys.forEach { kavling ->
+                mapListPembayaranLama[kavling]?.forEach { pembayaranWithNamaCostumer ->
+                    val cells = mutableListOf<RumCell>()
+                    val pembayaran = pembayaranWithNamaCostumer.pembayaran
+                    cells.add(RumCell(pembayaranWithNamaCostumer.namaCostumer))
+                    cells.add(RumCell(pembayaran.tanggal))
+                    cells.add(RumCell(pembayaran.termin))
+                    cells.add(RumCell(pembayaran.jumlahUangDibayar))
+
+                    if (cells.isNotEmpty()) {
+                        add(cells)
+                    }
+                }
+            }
+        }
+
+
+        adapter.setAllItems(columnHeader, rowHeaders, cellLists)
+        binding.tableviewRekapUangMasuk.apply {
+            setColumnWidth(0, 350) // Nama Costumer
+            setColumnWidth(1, 300) // Tanggal
+            setColumnWidth(2, 300) // Jenis Pembayaran
+            setColumnWidth(3, 350) // Jumlah Pembayaran
         }
 
         adapter.notifyDataSetChanged()
