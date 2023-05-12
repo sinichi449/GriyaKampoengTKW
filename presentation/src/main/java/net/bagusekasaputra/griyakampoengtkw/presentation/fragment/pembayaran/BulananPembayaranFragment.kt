6ead14ae03b01a5f15469a16c348eb41f64cd846
sinichi449 <bagusekasaputra460@gmail.com>
2023-05-13 03:26:14 +0700

    fix background Total Tunggakan text e kurang jelas

--- presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/fragment/pembayaran/BulananPembayaranFragment.kt
+++ presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/fragment/pembayaran/BulananPembayaranFragment.kt
@@ -1,5 +1,6 @@
 package net.bagusekasaputra.griyakampoengtkw.presentation.fragment.pembayaran
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.StrikethroughSpan
@@ -27,7 +28,7 @@ class BulananPembayaranFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentBulananPembayaranBinding.inflate(inflater, container, false)
 
@@ -68,18 +69,23 @@ class BulananPembayaranFragment : Fragment() {
         val absoluteTunggakanStr = NumberUtil.formatLongToString(totalTunggakan.absoluteValue)
 
         val textBackgroundColor: Int
+        val textTunggakan: String
         if (totalTunggakan <= 0) {
-            val textTunggakan = (if (totalTunggakan == 0L) "Rp" else "- Rp. ") + absoluteTunggakanStr
+            textTunggakan = (if (totalTunggakan == 0L) "Rp" else "- Rp. ") + absoluteTunggakanStr
             textBackgroundColor = ContextCompat.getColor(requireContext(), R.color.pembayaran_bulanan_lunas)
 
             binding.tvTotalTunggakan.text = SpannableString(textTunggakan).apply {
                 setSpan(StrikethroughSpan(), 0, textTunggakan.length, 0)
             }
         } else {
-            val textTunggakan = "Rp. $absoluteTunggakanStr"
-            textBackgroundColor = ContextCompat.getColor(requireContext(), R.color.pembayaran_bulanan_belum_lunas)
+            textTunggakan = "Rp. $absoluteTunggakanStr"
+            textBackgroundColor = ContextCompat.getColor(requireContext(), R.color.pembayaran_bulanan_belum_lunas_bg_total)
 
             binding.tvTotalTunggakan.text = textTunggakan
+            binding.tvTotalTunggakan.setTextColor(ContextCompat.getColor(
+                requireContext(), R.color.white
+            ))
+            binding.tvTotalTunggakan.typeface = Typeface.DEFAULT_BOLD
         }
         binding.layoutBackgroundPelunasan.setBackgroundColor(textBackgroundColor)
     }
