8c531cd7dcf8db8d86066777d263fb3d02cad44e
sinichi449 <bagusekasaputra460@gmail.com>
2023-05-14 14:14:42 +0700

    full tabel pembayaran oke

--- presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/activities/PembayaranTabelFullActivity.kt
+++ presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/activities/PembayaranTabelFullActivity.kt
@@ -9,9 +9,11 @@ import androidx.activity.viewModels
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import net.bagusekasaputra.griyakampoengtkw.presentation.R
+import net.bagusekasaputra.griyakampoengtkw.presentation.adapter.viewpager.TabelPembayaranViewPagerAdapter
 import net.bagusekasaputra.griyakampoengtkw.presentation.custom.TabelPembayaranNavHelper
 import net.bagusekasaputra.griyakampoengtkw.presentation.databinding.ActivityPembayaranTabelFullBinding
 import net.bagusekasaputra.griyakampoengtkw.presentation.viewmodel.FormPembayaranViewModel
+import net.bagusekasaputra.griyakampoengtkw.presentation.viewmodel.FormPembayaranViewModel.TablePembayaranType
 
 @SuppressLint("SetTextI18n")
 @AndroidEntryPoint
@@ -30,31 +32,35 @@ class PembayaranTabelFullActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val kavling = intent?.extras?.getString(EXTRAS_KAVLING_KODE)
-
         if (!kavling.isNullOrEmpty()) {
             val snackBarLoading = Snackbar.make(binding.root, "Mendapatkan List Pembayaran ...", Snackbar.LENGTH_INDEFINITE)
             pembayaranViewModel.getListPembayaranBulanan(
                 kavling,
-                onLoading = {
-                    snackBarLoading.show()
-                },
-                onSuccess = {
-                    snackBarLoading.dismiss()
-                },
+                onLoading = { snackBarLoading.show() },
+                onSuccess = { snackBarLoading.dismiss() },
                 onFailure = {
                     Toast.makeText(this, it, Toast.LENGTH_LONG).show()
                 },
             )
+        }
 
-            TabelPembayaranNavHelper(
-                lifecycleOwner = this,
-                fragmentManager = supportFragmentManager,
-                pembayaranViewModel = pembayaranViewModel,
-                containerId = R.id.navHostFragment_fullscreen_pembayaran,
-                triggerViews = arrayOf(
-                    binding.fabSwitchTabel
-                )
-            )
+        val tabelNavHelper = TabelPembayaranNavHelper(
+            lifecycleOwner = this,
+            fragmentManager = supportFragmentManager,
+            pembayaranViewModel = pembayaranViewModel,
+            containerId = R.id.navHostFragment_full_pembayaran,
+            triggerViews = arrayOf(
+                binding.btnSwitchTabel
+            ),
+        )
+        tabelNavHelper.listener = object : TabelPembayaranNavHelper.TabelPembayaranListener {
+            override fun onTabelChanged(tableType: TablePembayaranType) {
+                binding.btnSwitchTabel?.text = when (tableType) {
+                    TablePembayaranType.FORM_PEMBAYARAN -> "Per Bulan"
+                    TablePembayaranType.PEMBAYARAN_BULANAN -> "Semua"
+                }
+            }
+
         }
     }
 
