23c2358e4f58bb5de08bc62555d211ed16b00388
sinichi449 <bagusekasaputra460@gmail.com>
2023-05-18 00:40:47 +0700

    stepper log status pembayaran oke, kurang tint Icon e tok dan tabel LogPengembalian

--- presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/custom/StatusPembayaranLayoutHelper.kt
+++ presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/custom/StatusPembayaranLayoutHelper.kt
@@ -1,11 +1,19 @@
 package net.bagusekasaputra.griyakampoengtkw.presentation.custom
 
 import android.annotation.SuppressLint
-import android.graphics.PorterDuff
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
 import androidx.core.content.ContextCompat
+import moe.feng.common.stepperview.IStepperAdapter
+import moe.feng.common.stepperview.VerticalStepperItemView
+import moe.feng.common.stepperview.VerticalStepperView
 import net.bagusekasaputra.griyakampoengtkw.domain.entity.statusPembayaran.StatusPembayaran
 import net.bagusekasaputra.griyakampoengtkw.presentation.R
 import net.bagusekasaputra.griyakampoengtkw.presentation.databinding.LayoutStatusPembayaranBinding
+import net.bagusekasaputra.griyakampoengtkw.presentation.databinding.LayoutStepperLogStatusPembayaranBinding
+import net.bagusekasaputra.griyakampoengtkw.presentation.toSlashedDate
 
 class StatusPembayaranLayoutHelper(
     private val binding: LayoutStatusPembayaranBinding
@@ -15,52 +23,130 @@ class StatusPembayaranLayoutHelper(
     fun onLoading() {
         setDrawableIndikator(R.drawable.indicator_status_pembayaran, android.R.color.darker_gray)
         binding.tvStatus.text = "Memuat status pembayaran ..."
+
+        binding.root.isClickable = false
+        binding.layoutStepper.visibility = View.GONE
     }
 
-    @Suppress("DEPRECATION")
     fun onSuccess(statusPembayaran: StatusPembayaran) {
-        // TODO: show all list
         val currentStatus = statusPembayaran.currentStatus
 
         val indikatorTint = getIndicatorTintColor(currentStatus)
-        val textStatus = getStringStatus(currentStatus)
+        val textStatus = currentStatus.getTitle()
 
         setDrawableIndikator(R.drawable.indicator_status_pembayaran, indikatorTint)
         binding.tvStatus.text = textStatus
+
+        binding.layoutStepper.visibility = View.GONE
+        binding.verticalStepperLogStatusPembayaran.stepperAdapter = LogStatusPembayaranStepperAdapter(
+            binding.verticalStepperLogStatusPembayaran,
+            statusPembayaran.listStatus
+        )
+
+        binding.root.isCheckable = true
+        
+        binding.root.setOnClickListener {
+            val layoutStepperVisibility = binding.layoutStepper.visibility
+            binding.layoutStepper.visibility = if (layoutStepperVisibility == View.VISIBLE)
+                View.GONE else View.VISIBLE
+        }
     }
 
     fun onFailure() {
         setDrawableIndikator(R.drawable.baseline_warning_24, R.color.abang)
+
+        binding.root.isClickable = false
+        binding.layoutStepper.visibility = View.GONE
     }
 
     private fun setDrawableIndikator(drawableResId: Int, tintColor: Int) {
-        val drawableIndikator = ContextCompat.getDrawable(binding.root.context, drawableResId)
-        drawableIndikator?.setTint(getColor(tintColor))
+        val context = binding.root.context
+        val drawableIndikator = ContextCompat.getDrawable(context, drawableResId)
+        drawableIndikator?.setTint(getColor(context, tintColor))
 
         binding.imgIndikatorStatus.background = drawableIndikator
     }
 
-    private fun getIndicatorTintColor(status: StatusPembayaran.Status): Int {
-        return when (status) {
-            is StatusPembayaran.Nil -> R.color.status_pembayaran_nil
-            is StatusPembayaran.Aktif -> R.color.status_pembayaran_aktif
-            is StatusPembayaran.Suspend -> R.color.status_pembayaran_suspend
-            is StatusPembayaran.Jeda -> R.color.status_pembayaran_jeda
-            is StatusPembayaran.Batal -> R.color.status_pembayaran_batal
+    companion object {
+        private fun getIndicatorTintColor(status: StatusPembayaran.Status): Int {
+            return when (status) {
+                is StatusPembayaran.Nil -> R.color.status_pembayaran_nil
+                is StatusPembayaran.Aktif -> R.color.status_pembayaran_aktif
+                is StatusPembayaran.Suspend -> R.color.status_pembayaran_suspend
+                is StatusPembayaran.Jeda -> R.color.status_pembayaran_jeda
+                is StatusPembayaran.Batal -> R.color.status_pembayaran_batal
+            }
+        }
+
+        private fun getColor(context: Context, resId: Int): Int {
+            return ContextCompat.getColor(context, resId)
         }
     }
 
-    private fun getStringStatus(status: StatusPembayaran.Status): String {
-        return when (status) {
-            is StatusPembayaran.Nil -> "NIL"
-            is StatusPembayaran.Aktif -> "Aktif"
-            is StatusPembayaran.Suspend -> "Suspend"
-            is StatusPembayaran.Jeda -> "Jeda"
-            is StatusPembayaran.Batal -> "Batal"
-        }
-    }
 
-    private fun getColor(resId: Int): Int {
-        return ContextCompat.getColor(binding.root.context, resId)
+    class LogStatusPembayaranStepperAdapter(
+        private val stepperView: VerticalStepperView,
+        private val statuses: List<StatusPembayaran.Status>,
+    ): IStepperAdapter {
+        override fun getTitle(position: Int): CharSequence {
+            return statuses[position].getTitle()
+        }
+
+        override fun getSummary(position: Int): CharSequence {
+            return statuses[position].tanggal.toSlashedDate()
+        }
+
+        override fun size(): Int {
+            return statuses.size
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun onCreateCustomView(position: Int, context: Context?, parent: VerticalStepperItemView?): View {
+            val layoutInflater = LayoutInflater.from(context)
+            val stepperBinding = LayoutStepperLogStatusPembayaranBinding.inflate(layoutInflater, parent, false)
+
+            val status = statuses[position]
+
+            val doneIconTint = getIndicatorTintColor(status)
+            parent?.doneIcon?.setTint(getColor(context!!, doneIconTint))
+
+            stepperBinding.itemContent.text = StringBuilder().apply {
+                if (status.keterangan.isNotEmpty()) {
+                    append(status.keterangan)
+                    append("\n\n")
+                }
+                if (status is StatusPembayaran.Batal) {
+                    append("Berikut Tabel Pengembalian uang:")
+                    append("\n\n")
+                    append("INI TABEL!")
+                    append("\n\n")
+                    append("Pengganti untuk unit ini adalah ${status.costumerPengganti.nama}.")
+                    append("\n\n")
+                }
+                append("Perubahan terakhir pada ${status.tanggal.toSlashedDate()}")
+            }.toString()
+
+            stepperBinding.buttonNext.setOnClickListener {
+                stepperView.nextStep()
+            }
+            stepperBinding.buttonPrev.setOnClickListener {
+                if (position != 0) {
+                    stepperView.prevStep()
+                } else {
+                    stepperView.isAnimationEnabled = !stepperView.isAnimationEnabled
+                }
+            }
+
+            return stepperBinding.root
+        }
+
+        override fun onShow(p0: Int) {
+
+        }
+
+        override fun onHide(p0: Int) {
+
+        }
+
     }
 }
