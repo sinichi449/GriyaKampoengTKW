fd1826fb0ef5e11f06f84372da7772d4ac1c0af3
sinichi449 <bagusekasaputra460@gmail.com>
2023-05-25 23:50:07 +0700

    masking list pembayaran oke

--- domain/src/main/java/net/bagusekasaputra/griyakampoengtkw/domain/asyncUseCase/indenBooking/GetAllPembayaranIndenBookingAsyncUseCase.kt
+++ domain/src/main/java/net/bagusekasaputra/griyakampoengtkw/domain/asyncUseCase/indenBooking/GetAllPembayaranIndenBookingAsyncUseCase.kt
@@ -15,9 +15,28 @@ class GetAllPembayaranIndenBookingAsyncUseCase(
 
     override fun process(request: Request): Flow<Result<List<Pembayaran>?>> {
         return flow {
-            val result = indenBookingRepository.getAllPembayaran(request.keyId, DataMode.ONLINE)
+            val pembayaranList = indenBookingRepository
+                .getAllPembayaran(request.keyId, DataMode.ONLINE)
+                .getOrThrow()
+            val hargaRumah = indenBookingRepository.getHargaRumah(request.keyId, DataMode.ONLINE)
+                .getOrThrow()
 
-            emit(result)
+            // Masking pembayaran: Total uang masuk, persentase, etc
+            val pseudoHargaKavling = hargaRumah?.toHargaKavling("D99")
+            val maskedPembayaran = pembayaranList?.let {
+                if (pseudoHargaKavling != null) {
+                    Pembayaran.maskPembayaran(it, pseudoHargaKavling,
+                        onCekFotoPembayaran = {
+                            // TODO: Cek foto pembayaran
+                            false
+                        }
+                    )
+                } else {
+                    null
+                }
+            }
+
+            emit(Result.success(maskedPembayaran))
         }
     }
 }
--- domain/src/main/java/net/bagusekasaputra/griyakampoengtkw/domain/entity/indenBooking/HargaRumahIndenBooking.kt
+++ domain/src/main/java/net/bagusekasaputra/griyakampoengtkw/domain/entity/indenBooking/HargaRumahIndenBooking.kt
@@ -1,5 +1,8 @@
 package net.bagusekasaputra.griyakampoengtkw.domain.entity.indenBooking
 
+import net.bagusekasaputra.griyakampoengtkw.domain.NumberUtil
+import net.bagusekasaputra.griyakampoengtkw.domain.entity.HargaKavling
+
 /**
  * Inden-Booking only
  */
@@ -7,4 +10,13 @@ data class HargaRumahIndenBooking(
     val harga: Long,
     val tambahLuasan: Long,
     var keyId: String = "",
-)
+) {
+
+    fun toHargaKavling(kavling: String): HargaKavling {
+        return HargaKavling(
+            kavlingKode = kavling,
+            harga = NumberUtil.formatLongToString(harga),
+            tambahanLuas = NumberUtil.formatLongToString(tambahLuasan),
+        )
+    }
+}
