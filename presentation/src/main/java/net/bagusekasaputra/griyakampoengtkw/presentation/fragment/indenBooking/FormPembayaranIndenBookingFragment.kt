968602ba0c461d77f2147074e000401f5eea16c0
sinichi449 <bagusekasaputra460@gmail.com>
2023-05-31 04:11:56 +0700

    added tambah harga rumah dialog

--- presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/fragment/indenBooking/FormPembayaranIndenBookingFragment.kt
+++ presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/fragment/indenBooking/FormPembayaranIndenBookingFragment.kt
@@ -1,5 +1,6 @@
 package net.bagusekasaputra.griyakampoengtkw.presentation.fragment.indenBooking
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,12 +8,22 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import net.bagusekasaputra.griyakampoengtkw.domain.NumberUtil
 import net.bagusekasaputra.griyakampoengtkw.domain.entity.Pembayaran
 import net.bagusekasaputra.griyakampoengtkw.presentation.activity.DetailIndenBookingActivity
+import net.bagusekasaputra.griyakampoengtkw.presentation.custom.ThousandSeparatorTextWatcher
+import net.bagusekasaputra.griyakampoengtkw.presentation.databinding.DialogEditHargaRumahIndenBookingBinding
 import net.bagusekasaputra.griyakampoengtkw.presentation.databinding.FragmentFormPembayaranIndenBookingBinding
 import net.bagusekasaputra.griyakampoengtkw.presentation.tableview.formPembayaran.FullPembayaranTableWrapper
+import net.bagusekasaputra.griyakampoengtkw.presentation.util.DialogUtil
+import net.bagusekasaputra.griyakampoengtkw.presentation.util.InputUtil
 import net.bagusekasaputra.griyakampoengtkw.presentation.util.UiUtils
 import net.bagusekasaputra.griyakampoengtkw.presentation.viewmodel.IndenBookingViewModel
 
@@ -46,6 +57,10 @@ class FormPembayaranIndenBookingFragment : Fragment() {
         with((requireActivity() as DetailIndenBookingActivity).getFab()) {
             // Hide on scroll
             UiUtils.hideFabsOnVerticalScroll(binding.scrollViewPembayaranIndenBooking, this)
+        }
+
+        binding.imgEditHargaRumah.setOnClickListener {
+            dialogEditHargaRumah()
         }
 
         sync()
@@ -102,6 +117,55 @@ class FormPembayaranIndenBookingFragment : Fragment() {
                     Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
                 }
             )
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun dialogEditHargaRumah() {
+        val dialogBinding = DialogEditHargaRumahIndenBookingBinding.inflate(layoutInflater)
+        val dialogView = MaterialAlertDialogBuilder(requireContext()).apply {
+            setView(dialogBinding.root)
+        }.create()
+
+        DialogUtil.additionalDialogSetting(requireContext(), dialogView)
+        dialogView.show()
+
+        dialogBinding.edtHarga.apply {
+            val harga = binding.tvHarga.text
+            if (harga != "0")
+                this.setText(harga)
+            addTextChangedListener(ThousandSeparatorTextWatcher(this))
+        }
+        dialogBinding.edtTambahLuasan.apply {
+            val tambahanLuas = binding.tvTambahanLuas.text
+            if (tambahanLuas != "0") this.setText(tambahanLuas)
+
+            addTextChangedListener(ThousandSeparatorTextWatcher(this))
+        }
+
+        dialogBinding.btnTambahkan.setOnClickListener {
+            val isInvalidEdt = InputUtil.isNullOrEmptyEditTexts(dialogBinding.edtHarga)
+
+            if (!isInvalidEdt) {
+                lifecycleScope.launch(Dispatchers.Default) {
+                    withContext(Dispatchers.Main) {
+                        dialogBinding.btnTambahkan.startAnimation()
+                    }
+
+                    delay(5000L)
+
+                    withContext(Dispatchers.Main) {
+                        dialogView.dismiss()
+                    }
+                }
+
+            } else {
+                Toast.makeText(requireContext(), "Input belum benar!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        dialogBinding.btnBatal.setOnClickListener {
+            dialogView.dismiss()
         }
     }
 
