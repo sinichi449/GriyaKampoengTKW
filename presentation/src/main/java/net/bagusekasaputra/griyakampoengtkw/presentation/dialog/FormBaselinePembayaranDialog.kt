e16e29929f0682bc7d36c1e9f4278441cb4c8211
sinichi449 <bagusekasaputra460@gmail.com>
2023-06-03 13:35:55 +0700

    bugfix pilih tanggal hitung angsuran bulanan

--- presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/dialog/FormBaselinePembayaranDialog.kt
+++ presentation/src/main/java/net/bagusekasaputra/griyakampoengtkw/presentation/dialog/FormBaselinePembayaranDialog.kt
@@ -9,6 +9,7 @@ import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import net.bagusekasaputra.griyakampoengtkw.domain.DateUtil.toStringAndLongBulan
 import net.bagusekasaputra.griyakampoengtkw.domain.NumberUtil
@@ -45,176 +46,185 @@ class FormBaselinePembayaranDialog(
 
         DialogUtil.additionalDialogSetting(requireContext(), dialog)
 
-        binding.edtInfoHargaKavling.setText("Rp. ${NumberUtil.formatLongToString(hargaKavling.hargaLong)}")
-        binding.spinnerTimeframeAngsuran.apply {
-            val listOpsiTimeframe = mutableListOf<String>().apply {
-                add(BaselinePembayaran.OPSI_TIMEFRAME_BULAN, "Bulan")
-                add(BaselinePembayaran.OPSI_TIMEFRAME_TAHUN, "Tahun")
-            }
-            adapter = ArrayAdapter(
-                requireContext(),
-                android.R.layout.simple_spinner_dropdown_item,
-                listOpsiTimeframe
-            )
+        val listOpsiTimeframe = mutableListOf<String>().apply {
+            add(BaselinePembayaran.OPSI_TIMEFRAME_BULAN, "Bulan")
+            add(BaselinePembayaran.OPSI_TIMEFRAME_TAHUN, "Tahun")
         }
-
         val isEditMode = baselinePembayaran != null
-        if (isEditMode) {
-            binding.tvDialogTitle.text = "Ubah Angsuran Bulanan"
-            binding.btnTambahkan.text = "Ubah"
-            baselinePembayaran?.also {
-                binding.edtOpsiTimeframeAngsuran.setText(it.opsiBulan.toString())
-                binding.spinnerTimeframeAngsuran.setSelection(BaselinePembayaran.OPSI_TIMEFRAME_BULAN) // Default to "Bulan"
-                binding.edtUangAngsuranPerBulan.setText(it.jumlahUang.toString())
 
-                binding.tvPerhitungan.text = StringBuilder().apply {
-                    val parsedHargaKavling = NumberUtil.formatLongToString(hargaKavling.hargaLong)
-                    val uangAngsuran = BaselinePembayaran.hitungAngsuranPerBulan(
-                        hargaKavling, BaselinePembayaran.OPSI_TIMEFRAME_BULAN,
-                        it.opsiBulan
-                    ).let { parsedUangAngsuran ->
-                        NumberUtil.formatDoubleToString(parsedUangAngsuran)
-                    }
+        with(binding) {
+            edtInfoHargaKavling.setText("Rp. ${NumberUtil.formatLongToString(hargaKavling.hargaLong)}")
 
-                    append("$parsedHargaKavling ")
-                    append("/ ${it.opsiBulan} Bulan ")
-                    append("= Rp. $uangAngsuran")
-                }.toString()
+            spinnerTimeframeAngsuran.adapter = ArrayAdapter(
+                requireContext(), android.R.layout.simple_spinner_dropdown_item, listOpsiTimeframe
+            )
 
-                binding.tvInfoParsedUangAngsuranRupiah.text = "= Rp. ${it.parsedJumlahUang}"
-                binding.edtMaksimalTanggalPembayaran.setText(it.tanggalPembayaranMaks.toString())
+            edtTanggalPembelian.apply {
+                val datePickerHelper = DatePickerHelper(requireContext(), binding.btnPilihTanggalPembelian, this)
 
                 if (!tanggalPembelian.isNullOrEmpty()) {
-                    binding.tvTanggalAngsuranSelesai.text = BaselinePembayaran
-                        .hitungTanggalAngsuranSelesai(
-                            tanggalPembelian.toDate(),
-                            BaselinePembayaran.OPSI_TIMEFRAME_BULAN, it.opsiBulan
-                        )
-                        .toStringAndLongBulan()
+                    setText(tanggalPembelian)
+
+                    datePickerHelper.setupDateDefaultOrPick(false)
                 } else {
-                    binding.tvTanggalAngsuranSelesai.text = "-"
+                    datePickerHelper.setupDateDefaultOrPick(true)
                 }
             }
-        } else {
-            binding.tvDialogTitle.text = "Tambah Angsuran Bulanan"
-            binding.btnTambahkan.text = "Tambahkan"
-            binding.tvTanggalAngsuranSelesai.text = "-"
-        }
 
-        binding.edtTanggalPembelian.apply {
-            if (!tanggalPembelian.isNullOrEmpty()) {
-                setText(tanggalPembelian)
-            } else {
-                DatePickerHelper(requireContext(), binding.btnPilihTanggalPembelian, this)
-            }
-        }
+            edtUangAngsuranPerBulan.addTextChangedListener {
+                it?.toString()?.also { string ->
+                    if (string.isNotEmpty()) {
+                        val text = "= Rp. ${NumberUtil.formatDoubleToString(string.toDouble())}"
 
-        binding.btnHitung.setOnClickListener {
-            val isInvalidEdt = InputUtil.isNullOrEmptyEditTexts(
-                binding.edtOpsiTimeframeAngsuran,
-                binding.edtTanggalPembelian,
-            )
-            if (!isInvalidEdt) {
-                val timeFrame = binding.edtOpsiTimeframeAngsuran.text.toString().toInt()
-                val opsiTimeFrame = binding.spinnerTimeframeAngsuran.selectedItemPosition
-                val biayaAngsuranPerBulan = try {
-                    BaselinePembayaran.hitungAngsuranPerBulan(hargaKavling, opsiTimeFrame, timeFrame)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-
-                    Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
-
-                    0.0
-                }
-                val tanggalAngsuranSelesai = binding.edtTanggalPembelian.text.toString().toDate().let {
-                    BaselinePembayaran.hitungTanggalAngsuranSelesai(it, opsiTimeFrame, timeFrame)
-                }
-
-                binding.tvPerhitungan.text = StringBuilder().run {
-                    val hargaKavling = NumberUtil.formatLongToString(hargaKavling.hargaLong)
-                    val timeFrameAngsuran = when (opsiTimeFrame) {
-                        BaselinePembayaran.OPSI_TIMEFRAME_TAHUN -> "$timeFrame Tahun"
-                        BaselinePembayaran.OPSI_TIMEFRAME_BULAN -> "$timeFrame Bulan"
-                        else -> "NULL"
-                    }
-
-                    append("$hargaKavling ")
-                    append("/ $timeFrameAngsuran ")
-                    append("= Rp. ${NumberUtil.formatDoubleToString(biayaAngsuranPerBulan)}")
-
-                    toString()
-                }
-
-                binding.edtUangAngsuranPerBulan.setText(biayaAngsuranPerBulan.let {
-                    DecimalFormat("#", DecimalFormatSymbols(Locale.US))
-                        .format(it)
-                    // Prevent 1E77 or alike (exponents)
-                })
-
-                binding.tvTanggalAngsuranSelesai.text = tanggalAngsuranSelesai.toStringAndLongBulan()
-            } else {
-                Toast.makeText(requireContext(), "Input masih belum benar!", Toast.LENGTH_LONG)
-                    .show()
-            }
-        }
-        binding.edtUangAngsuranPerBulan.addTextChangedListener {
-            it?.toString()?.also { string ->
-                if (string.isNotEmpty()) {
-                    val text = "= Rp. ${NumberUtil.formatDoubleToString(string.toDouble())}"
-
-                    binding.tvInfoParsedUangAngsuranRupiah.text = text
-                } else {
-                    binding.tvInfoParsedUangAngsuranRupiah.text = "Rp. 0"
-                }
-            }
-        }
-
-        binding.btnTambahkan.setOnClickListener {
-            val isInvalidInput = InputUtil.isNullOrEmptyEditTexts(
-                binding.edtOpsiTimeframeAngsuran,
-                binding.edtUangAngsuranPerBulan,
-                binding.edtMaksimalTanggalPembayaran,
-            )
-
-            if (!isInvalidInput) {
-                val opsiBulan = binding.spinnerTimeframeAngsuran.selectedItemPosition.let { position ->
-                    val numTimeFrame = binding.edtOpsiTimeframeAngsuran.text.toString().toInt()
-                    if (position == 0) {
-                        numTimeFrame * 12
+                        binding.tvInfoParsedUangAngsuranRupiah.text = text
                     } else {
-                        numTimeFrame
+                        binding.tvInfoParsedUangAngsuranRupiah.text = "Rp. 0"
                     }
                 }
-                val biayaAngsuran = binding.edtUangAngsuranPerBulan.text?.toString()?.toLong() ?: 0L
-                val tanggalPembayaranMaksimal = binding.edtMaksimalTanggalPembayaran.text.toString().toInt()
-                val baselinePembayaran = BaselinePembayaran(currentKavlingKode, opsiBulan, biayaAngsuran, tanggalPembayaranMaksimal)
-
-                pembayaranViewModel.insertBaselinePembayaran(
-                    baselinePembayaran = baselinePembayaran,
-                    onLoading = {
-                        binding.btnTambahkan.text = "Menyimpan ..."
-                        binding.btnTambahkan.isEnabled = false
-                    },
-                    onComplete = {
-                        Toast.makeText(requireContext(), "Berhasil mengubah Angsuran Bulanan: Rp ${NumberUtil.formatLongToString(biayaAngsuran)}", Toast.LENGTH_SHORT)
-                            .show()
-
-                        dialog.dismiss()
-                    },
-                    onFailure = {
-                        Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
-
-                        binding.btnTambahkan.isEnabled = true
-                        binding.btnTambahkan.text = if (isEditMode) "Ubah" else "Tambahkan"
-                    }
-                )
             }
-        }
-        binding.btnBatal.setOnClickListener {
-            // TODO: Cancel tambahkanJob
-            dialog.dismiss()
+
+            if (isEditMode) {
+                tvDialogTitle.text = "Ubah Angsuran Bulanan"
+                btnTambahkan.text = "Ubah"
+                baselinePembayaran?.also {
+                    edtOpsiTimeframeAngsuran.setText(it.opsiBulan.toString())
+                    spinnerTimeframeAngsuran.setSelection(BaselinePembayaran.OPSI_TIMEFRAME_BULAN) // Default to "Bulan"
+
+                    tvPerhitungan.text = keteranganPerhitunganAngsuran(
+                        hargaKavling, BaselinePembayaran.OPSI_TIMEFRAME_BULAN, it.opsiBulan
+                    )
+                    edtUangAngsuranPerBulan.setText(it.jumlahUang.toString())
+
+                    tvInfoParsedUangAngsuranRupiah.text = "= Rp. ${it.parsedJumlahUang}"
+                    edtMaksimalTanggalPembayaran.setText(it.tanggalPembayaranMaks.toString())
+
+                    if (!tanggalPembelian.isNullOrEmpty()) {
+                        tvTanggalAngsuranSelesai.text = BaselinePembayaran
+                            .hitungTanggalAngsuranSelesai(
+                                tanggalPembelian.toDate(),
+                                BaselinePembayaran.OPSI_TIMEFRAME_BULAN, it.opsiBulan
+                            )
+                            .toStringAndLongBulan()
+                    } else {
+                        tvTanggalAngsuranSelesai.text = "-"
+                    }
+                }
+            } else {
+                tvDialogTitle.text = "Tambah Angsuran Bulanan"
+                btnTambahkan.text = "Tambahkan"
+                tvTanggalAngsuranSelesai.text = "-"
+            }
+
+            btnHitung.setOnClickListener {
+                val isInvalidEdt = InputUtil.isNullOrEmptyEditTexts(
+                    edtOpsiTimeframeAngsuran, edtTanggalPembelian,
+                )
+                if (!isInvalidEdt) {
+                    val timeFrame = edtOpsiTimeframeAngsuran.text.toString().toInt()
+                    val opsiTimeFrame = spinnerTimeframeAngsuran.selectedItemPosition
+                    val biayaAngsuranPerBulan = try {
+                        BaselinePembayaran.hitungAngsuranPerBulan(hargaKavling, opsiTimeFrame, timeFrame)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+
+                        Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
+
+                        0.0
+                    }
+                    val tanggalAngsuranSelesai = edtTanggalPembelian.text.toString().toDate().let {
+                        BaselinePembayaran.hitungTanggalAngsuranSelesai(it, opsiTimeFrame, timeFrame)
+                    }
+
+                    tvPerhitungan.text = keteranganPerhitunganAngsuran(
+                        hargaKavling, opsiTimeFrame, timeFrame
+                    )
+
+                    edtUangAngsuranPerBulan.setText(biayaAngsuranPerBulan.let {
+                        // Prevent 1E77 or alike (exponents)
+                        DecimalFormat("#", DecimalFormatSymbols(Locale.US))
+                            .format(it)
+                    })
+
+                    tvTanggalAngsuranSelesai.text = tanggalAngsuranSelesai.toStringAndLongBulan()
+                } else {
+                    Toast.makeText(requireContext(), "Input masih belum benar!", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+
+            btnTambahkan.setOnClickListener {
+                val isInvalidInput = InputUtil.isNullOrEmptyEditTexts(
+                    edtOpsiTimeframeAngsuran, edtUangAngsuranPerBulan, edtMaksimalTanggalPembayaran,
+                )
+
+                if (!isInvalidInput) {
+                    val opsiBulan = spinnerTimeframeAngsuran.selectedItemPosition.let { position ->
+                        val timeFrame = edtOpsiTimeframeAngsuran.text.toString().toInt()
+
+                        when (position) {
+                            BaselinePembayaran.OPSI_TIMEFRAME_BULAN -> timeFrame
+                            BaselinePembayaran.OPSI_TIMEFRAME_TAHUN -> timeFrame * 12
+                            else -> 0
+                        }
+                    }
+                    val biayaAngsuran = edtUangAngsuranPerBulan.text?.toString()?.toLong() ?: 0L
+                    val tanggalPembayaranMaksimal = edtMaksimalTanggalPembayaran.text.toString().toInt()
+                    val baselinePembayaran = BaselinePembayaran(
+                        currentKavlingKode,
+                        opsiBulan,
+                        biayaAngsuran,
+                        tanggalPembayaranMaksimal
+                    )
+
+                    pembayaranViewModel.insertBaselinePembayaran(
+                        baselinePembayaran = baselinePembayaran,
+                        onLoading = {
+                            btnTambahkan.text = "Menyimpan ..."
+                            btnTambahkan.isEnabled = false
+                            btnBatal.isEnabled = false
+                        },
+                        onComplete = {
+                            dialog.dismiss()
+
+                            Snackbar.make(root, "Berhasil mengubah Angsuran Bulanan", Snackbar.LENGTH_SHORT)
+                                .show()
+                        },
+                        onFailure = {
+                            Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
+
+                            btnTambahkan.isEnabled = true
+                            btnTambahkan.text = if (isEditMode) "Ubah" else "Tambahkan"
+                            btnBatal.isEnabled = true
+                        }
+                    )
+                }
+            }
+
+            btnBatal.setOnClickListener {
+                dialog.dismiss()
+            }
         }
 
         return dialog
+    }
+
+    private fun keteranganPerhitunganAngsuran(
+        hargaKavling: HargaKavling,
+        opsiTimeFrame: Int,
+        timeFrame: Int,
+    ): String {
+        val parsedHargaKavling = NumberUtil.formatLongToString(hargaKavling.hargaLong)
+        val parsedUangAngsuran = BaselinePembayaran.hitungAngsuranPerBulan(
+            hargaKavling, opsiTimeFrame, timeFrame
+        ).let {
+            NumberUtil.formatDoubleToString(it)
+        }
+        val parsedOpsiTimeFrame = when (opsiTimeFrame) {
+            BaselinePembayaran.OPSI_TIMEFRAME_BULAN -> "Bulan"
+            BaselinePembayaran.OPSI_TIMEFRAME_TAHUN -> "Tahun"
+            else -> "NULL"
+        }
+
+        return "$parsedHargaKavling / $timeFrame $parsedOpsiTimeFrame = Rp. $parsedUangAngsuran"
     }
 }
