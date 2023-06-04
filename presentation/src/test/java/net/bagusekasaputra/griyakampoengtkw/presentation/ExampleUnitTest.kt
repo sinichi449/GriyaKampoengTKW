54601247fea830c3408ca358c7d69808746cf8a6
sinichi449 <bagusekasaputra460@gmail.com>
2023-06-04 13:35:17 +0700

    clean useless tests

--- presentation/src/test/java/net/bagusekasaputra/griyakampoengtkw/presentation/ExampleUnitTest.kt
+++ presentation/src/test/java/net/bagusekasaputra/griyakampoengtkw/presentation/ExampleUnitTest.kt
@@ -1,12 +1,7 @@
 package net.bagusekasaputra.griyakampoengtkw.presentation
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.math.BigDecimal
-import java.math.RoundingMode
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -17,44 +12,5 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
-    }
-
-    @Test
-    fun test_menghilangkan_spasi() {
-        val inputText = "biaya pengiriman surat "
-
-        val lastIndex = inputText.length - 1
-
-        var newInput = inputText
-        if (inputText[lastIndex] == ' ')
-            newInput = inputText.substring(startIndex = 0, endIndex = lastIndex)
-
-        val expected = "biaya pengiriman surat"
-        assertEquals(expected, newInput)
-    }
-
-    @Test
-    fun test_progress_bar() {
-        val totalUnit = BigDecimal(43)
-
-        val progress = flow<Int> {
-            (1..43).forEach { index ->
-                val numProcessed = BigDecimal(index)
-
-                val progress = (numProcessed.divide(totalUnit, 2, RoundingMode.HALF_UP)
-                    .multiply(BigDecimal(100)))
-                    .toInt()
-
-                emit(progress)
-
-                delay(750)
-            }
-        }
-
-        runBlocking {
-            progress.collect {
-                println("Progress -> $it")
-            }
-        }
     }
 }
