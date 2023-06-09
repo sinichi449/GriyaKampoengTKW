4bb8fa95a604a90e2323f93b2e048ef9dfad0b4b
sinichi449 <bagusekasaputra460@gmail.com>
2023-06-10 04:59:05 +0700

    created KavlingAndProgressTest

--- domain/src/main/java/net/bagusekasaputra/griyakampoengtkw/domain/entity/kavling/KavlingAndProgress.kt
+++ domain/src/main/java/net/bagusekasaputra/griyakampoengtkw/domain/entity/kavling/KavlingAndProgress.kt
@@ -3,7 +3,9 @@ package net.bagusekasaputra.griyakampoengtkw.domain.entity.kavling
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import net.bagusekasaputra.griyakampoengtkw.domain.entity.Kavling
+import net.bagusekasaputra.griyakampoengtkw.domain.entity.KavlingSorter
 import net.bagusekasaputra.griyakampoengtkw.domain.entity.ProgressKavling
+import net.bagusekasaputra.griyakampoengtkw.domain.entity.SingleBlockKavlingSorter
 
 data class KavlingAndProgress(
     val blok: String,
@@ -17,6 +19,51 @@ data class KavlingAndProgress(
             newList.add(this)
 
             newList
+        }
+    }
+
+    companion object {
+        fun List<KavlingAndProgress>.containsMultipleBloks(blok: String): Boolean {
+            var result = false
+            for (item in this) {
+                if (item.blok != blok) {
+                    result = true
+                    break
+                }
+            }
+
+            return result
+        }
+
+        fun List<KavlingAndProgress>.sortByKavling(
+            blok: String,
+            sorter: KavlingSorter = SingleBlockKavlingSorter()
+        ): List<KavlingAndProgress> {
+            if (this.containsMultipleBloks(blok)) {
+                throw UnsupportedOperationException("Belum mendukung pengurutan Kavling yang terdiri dari blok yang berbeda!")
+            } else if (this.isEmpty()) {
+                return emptyList()
+            } else {
+                val map = buildMap {
+                    this@sortByKavling.forEach { item ->
+                        put(item.kavling, item)
+                    }
+                }
+                val kavlingsOnly = buildList {
+                    this@sortByKavling.forEach { item ->
+                        add(item.kavling)
+                    }
+                }
+                val sortedKavlings = Kavling.sortKavling(kavlingsOnly, sorter)
+
+                val result = buildList {
+                    sortedKavlings.forEach {
+                        val kavlingAndProgress = map[it]!!
+                        add(kavlingAndProgress)
+                    }
+                }
+                return result
+            }
         }
     }
 
---
+++ domain/src/test/java/net/bagusekasaputra/griyakampoengtkw/domain/entity/KavlingAndProgressTest.kt
@@ -0,0 +1,62 @@
+package net.bagusekasaputra.griyakampoengtkw.domain.entity
+
+import net.bagusekasaputra.griyakampoengtkw.domain.entity.kavling.KavlingAndProgress
+import net.bagusekasaputra.griyakampoengtkw.domain.entity.kavling.KavlingAndProgress.Companion.sortByKavling
+import org.junit.Assert
+import org.junit.Test
+import kotlin.random.Random
+
+class KavlingAndProgressTest {
+
+    @Test
+    fun givenUnsortedListOfThis_shouldSortedByKavling() {
+        val blok = "C"
+        val sizeKavling = randomKavlingSize()
+        val sortedItems = buildList {
+            repeat(sizeKavling) {
+                val numKavling = it + 1
+                val kode = "${blok}${numKavling}"
+                val kavling = Kavling.EMPTY(kode)
+                val progressKavling = ProgressKavling.EMPTY(kode)
+
+                add(KavlingAndProgress(
+                    blok = blok,
+                    kavling = kavling,
+                    progress = progressKavling,
+                ))
+            }
+        }
+        val shuffledItems = sortedItems.shuffled()
+
+        Assert.assertEquals(sortedItems, shuffledItems.sortByKavling(blok))
+    }
+
+    @Test
+    fun givenMultipleBlokListProvided_whenSortingListOfThis_shouldThrowUnsupportedOperationException() {
+        val bloks = listOf("A", "B")
+        val kavlingSize =  randomKavlingSize()
+        val multipleBlokList = buildList {
+            repeat(kavlingSize) { index ->
+                // take the first `bloks`'s element when `index` is an even number
+                // or take the second element when it is an odd number.
+                val blok = if (index % 2 == 0)
+                    bloks[0] else bloks[1]
+
+                val numKavling = index + 1
+                val kode = "${blok}${numKavling}"
+
+                add(KavlingAndProgress(
+                    blok = blok,
+                    kavling = Kavling.EMPTY(kode),
+                    progress = ProgressKavling.EMPTY(kode),
+                ))
+            }
+        }
+
+        Assert.assertThrows(UnsupportedOperationException::class.java) {
+            multipleBlokList.sortByKavling(bloks.first())
+        }
+    }
+
+    private fun randomKavlingSize() = Random.nextInt(from = 5, until = 20)
+}
