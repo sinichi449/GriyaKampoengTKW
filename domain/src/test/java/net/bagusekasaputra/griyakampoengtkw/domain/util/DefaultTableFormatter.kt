aedeedac07cce186fb509e8b0051c8b0cb31162d
sinichi449 <bagusekasaputra460@gmail.com>
2023-06-10 19:14:33 +0700

    add documentation

--- domain/src/test/java/net/bagusekasaputra/griyakampoengtkw/domain/util/DefaultTableFormatter.kt
+++ domain/src/test/java/net/bagusekasaputra/griyakampoengtkw/domain/util/DefaultTableFormatter.kt
@@ -1,5 +1,21 @@
 package net.bagusekasaputra.griyakampoengtkw.domain.util
 
+/**
+ * Convert your data into a console-style table.
+ *
+ * @param columnHeaderItems specify your Column Header in a [List] of [String].
+ * @param rowHeaderItems specify your Row Header content in a [List] of [Int].
+ * Note that currently this class only support for numeric Row Header.
+ * @param cellItems convert your class and its attribute into a [List] of [String], where
+ * each element of the [List] would correspond to, for example, your whole class properties/attributes.
+ * @param whiteSpaces the number of whitespaces to separate each column. See [Builder.getChPattern]
+ * for more information about [whiteSpaces].
+ * @param chPattern the pattern which would be followed to produce the Column Header. See [Builder.getChPattern]
+ * for more information about [chPattern].
+ * @param rhPattern the pattern which would be followed to produce the Row Header. See [Builder.getChPattern]
+ * for more information about [rhPattern].
+ *
+ */
 class DefaultTableFormatter private constructor(
     private val columnHeaderItems: List<String>,
     private val rowHeaderItems: List<Int>,
@@ -13,11 +29,10 @@ class DefaultTableFormatter private constructor(
         const val NUM_WHITESPACES = 15
         const val UNIT_WORD_FORMAT = "s"
         const val UNIT_NUMBER_FORMAT = "d"
-        const val FORMAT_COLUMN_HEADER = "%-15s%-15s%-15s%-15s\n"
-        const val FORMAT_ROWS = "%-15d%-15s%-15s%-15s\n"
     }
 
     override fun print() {
+        println()
         createColumnHeader()
         println(columnAndCellSeparator())
         createRows()
@@ -38,7 +53,7 @@ class DefaultTableFormatter private constructor(
 
     private fun columnAndCellSeparator(): String {
         return buildString {
-            repeat(whiteSpaces * columnHeaderItems.size + 5) {
+            repeat(whiteSpaces * (columnHeaderItems.size + 1)) {
                 append("-")
             }
         }
@@ -82,6 +97,27 @@ class DefaultTableFormatter private constructor(
             )
         }
 
+        /**
+         * A single unit of a Column Header follows below formula:
+         *
+         * **`%-{W}{F}`**
+         *
+         * where **{W}** is the number of whitespaces to separate the columns, and **{F}** is data format at
+         * that column. The **{F}** could be a numeric format, thus corresponds to regex pattern **`d`**
+         * _(digit)_, or whitespaces format **`s`**, which denotes for regex of, for example, a _tabs_  or a
+         * _space_ keystroke.
+         *
+         * This doesn't constrained to just a single pattern. You can concatenate them.
+         *
+         * **For example**: **%-13d** would means _create a digit format and add 13 whitespaces after it_.
+         * A bit more complex example would be **%-13d%-13s%-13s\n** would means _create a row with three
+         * columns, where for each columns add 13 whitespaces after each of it, and print a new line **(\n)**
+         * at the end of this row_.
+         *
+         * @param chSize pass your [Collection] subclass and call [Collection.size]. This is where I
+         * would create a pattern for your [chSize] of columns.
+         *
+         */
         private fun getChPattern(chSize: Int): String {
             return buildString {
                 repeat(chSize) {
@@ -91,6 +127,11 @@ class DefaultTableFormatter private constructor(
             }
         }
 
+        /**
+         * Row Header pattern follows exactly the same principle as Column Header.
+         *
+         * @see getChPattern
+         */
         private fun getRhPattern(chSize: Int): String {
             return buildString {
                 repeat(chSize) {
