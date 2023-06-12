a2d0d8e43b4bad520b889fd6acda58594d42f551
sinichi449 <bagusekasaputra460@gmail.com>
2023-06-12 21:06:31 +0700

    created model for :data module

---
+++ data/src/main/java/net/bagusekasaputra/griyakampoengtkw/data/model/PengembalianModel.kt
@@ -0,0 +1,12 @@
+package net.bagusekasaputra.griyakampoengtkw.data.model
+
+data class PengembalianModel(
+    val keyId: String = "",
+    val kavling: String = "",
+    val namaCustomer: String = "",
+    val tanggal: String = "",
+    val jumlah: String = "",
+    val keterangan: String = "",
+    val uri: String = "",
+    val timeMillis: Long = 0L,
+)
